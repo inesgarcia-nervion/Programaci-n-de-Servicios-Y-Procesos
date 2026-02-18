fbcc5b79aadcbaa829919705d9c249d24720bf23
inesgarcia-nervion <ines.garcia@iesnervion.es>
2026-02-18 08:27:06 +0100

    Ejercicio 1

---
+++ Tema3/Ejercicios1/ej1.py
@@ -0,0 +1,30 @@
+import threading
+import time
+import random
+
+class mensaje(threading.Thread):
+    def __init__(self, nombre):
+        super().__init__()
+        self.nombre = nombre
+        self.trabajo_realizado = False
+
+    def run(self):
+        while True:
+            print(f"Soy {self.nombre} y estoy trabajando")
+            tiempo = random.randint(1, 10)
+            time.sleep(tiempo)
+            print(f"Soy {self.nombre} y he terminado de trabajar")
+            self.trabajo_realizado = True
+
+if __name__ == "__main__":
+    nombres = ["Inés", "Ángela", "Dylan", "Paula", "Ale"]
+    print("Soy el hilo principal")
+    hilos = []
+    for nombre in nombres:
+        nom = mensaje(nombre)
+        nom.daemon = True  # Para que el programa termine si el principal termina
+        nom.start()
+        hilos.append(nom)
+    while not all(hilo.trabajo_realizado for hilo in hilos):
+        time.sleep(0.5)
+    print("Fin del hilo principal.")
